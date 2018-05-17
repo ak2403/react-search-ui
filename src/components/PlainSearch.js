5659d153c171dbc27758724c00ac25c5973728c6
vbiarunp <arunpasupathi24@gmail.com>
2018-05-17 14:35:47 +0530

    UI changes for search

--- src/components/PlainSearch.js
+++ src/components/PlainSearch.js
@@ -5,10 +5,12 @@ class PlainSearch extends Component {
   constructor(props) {
     super();
     this.state = {
-      searchValue: ''
+      searchValue: '',
+      selectedValue: ''
     };
     this.searchInput = this.searchInput.bind(this);
     this.clearSearch = this.clearSearch.bind(this);
+    this.selected = this.selected.bind(this);
   }
 
   componentWillMount() {
@@ -21,10 +23,16 @@ class PlainSearch extends Component {
     });
   }
 
-  clearSearch() {
+  clearSearch(element) {
+    debugger
     this.setState({
       searchValue: ''
     });
+  }
+
+  selected(value) {
+    debugger
+    console.log(value)
   }
 
   render() {
@@ -34,9 +42,9 @@ class PlainSearch extends Component {
       <div className="plain-search-group">
         <input type='text' onChange={this.searchInput} onBlur={this.clearSearch} />
         <div className="plain-search-result-group">
-          {this.state.searchValue ? fetchData.map(element => {
+          {this.state.searchValue ? fetchData.map((element, index) => {
             if (element.title.toLowerCase().search(this.state.searchValue.toLowerCase()) !== -1) {
-              return <div className="plain-search-result">
+              return <div className="plain-search-result" key={index} onClick={() => this.selected(element)}>
                 <div className="result-title">{element.title} <i className="fa fa-star"></i> {element.score}</div>
                 <div className="result-genre">Genre: {element.genre}</div>
               </div>
